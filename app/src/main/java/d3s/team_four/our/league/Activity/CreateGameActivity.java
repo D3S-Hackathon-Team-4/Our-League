3896c26abeb408660ce7e9dad35baea9d6ddb87b
hyemdooly <hyemdooly@gmail.com>
2017-02-05 06:14:09 +0900

    commit

--- app/src/main/java/d3s/team_four/our/league/Activity/CreateGameActivity.java
+++ app/src/main/java/d3s/team_four/our/league/Activity/CreateGameActivity.java
@@ -3,6 +3,10 @@ package d3s.team_four.our.league.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import d3s.team_four.our.league.R;
 
@@ -15,6 +19,26 @@ public class CreateGameActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle(""); // 야구면 야구 축구면 축구, 인텐트 ㄲ
+        Spinner spinner = (Spinner) findViewById(R.id.member_spinner);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.member, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+
+        });
+
+
 
     }
 }
