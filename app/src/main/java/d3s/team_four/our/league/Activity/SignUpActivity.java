b7aa718df06711fe0ea703af814e45687f2e4bfd
hyemdooly <hyemdooly@gmail.com>
2017-02-04 19:44:15 +0900

    hihi

--- app/src/main/java/d3s/team_four/our/league/Activity/SignUpActivity.java
+++ app/src/main/java/d3s/team_four/our/league/Activity/SignUpActivity.java
@@ -14,7 +14,7 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.);
+        setContentView(R.layout.activity_signup);
 
 
 
