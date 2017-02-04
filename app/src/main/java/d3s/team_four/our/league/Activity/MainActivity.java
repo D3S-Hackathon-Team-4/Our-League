1f40d4933285f326fb460d0ce7aa3620797b1d82
hyemdooly <hyemdooly@gmail.com>
2017-02-05 01:10:35 +0900

    d

--- app/src/main/java/d3s/team_four/our/league/Activity/MainActivity.java
+++ app/src/main/java/d3s/team_four/our/league/Activity/MainActivity.java
@@ -1,49 +1,44 @@
 package d3s.team_four.our.league.Activity;
 
-import android.app.Activity;
-import android.os.Build;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Window;
-import android.view.WindowManager;
+import android.widget.TextView;
 
 import d3s.team_four.our.league.PagerAdapter;
 import d3s.team_four.our.league.R;
+import d3s.team_four.our.league.SportSpeciesFragment.BadmintonFragment;
+import d3s.team_four.our.league.SportSpeciesFragment.BaseballFragment;
+import d3s.team_four.our.league.SportSpeciesFragment.BasketballFragment;
+import d3s.team_four.our.league.SportSpeciesFragment.LOLFragment;
+import d3s.team_four.our.league.SportSpeciesFragment.SoccerFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LOLFragment.OnFragmentInteractionListener, BaseballFragment.OnFragmentInteractionListener,
+        SoccerFragment.OnFragmentInteractionListener, BasketballFragment.OnFragmentInteractionListener, BadmintonFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            setStatusBarColor(MainActivity.this, getColor(R.color.colorStatusBar));
-        }
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
         ViewPager viewpager = (ViewPager) findViewById(R.id.view_pager);
-        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager());
+        TextView textView = (TextView) findViewById(R.id.species_text);
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(), textView);
         viewpager.setAdapter(pagerAdapter);
 
 
 
     }
 
-    private void setStatusBarColor(Activity activity, int color) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = activity.getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.setStatusBarColor(color);
-        }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
-
-
-
-
 }
--- app/src/main/java/d3s/team_four/our/league/PagerAdapter.java
+++ app/src/main/java/d3s/team_four/our/league/PagerAdapter.java
@@ -3,6 +3,7 @@ package d3s.team_four.our.league;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.widget.TextView;
 
 import d3s.team_four.our.league.SportSpeciesFragment.BadmintonFragment;
 import d3s.team_four.our.league.SportSpeciesFragment.BaseballFragment;
@@ -14,13 +15,15 @@ import d3s.team_four.our.league.SportSpeciesFragment.SoccerFragment;
  * Created by songhyemin on 2017. 2. 4..
  */
 
-public class PagerAdapter extends FragmentPagerAdapter{
+public class PagerAdapter extends FragmentPagerAdapter {
 
     private int MAX_PAGE = 5;
     private Fragment curFragment = new Fragment();
+    private TextView v = null;
 
-    public PagerAdapter(FragmentManager fm) {
+    public PagerAdapter(FragmentManager fm, TextView v) {
         super(fm);
+        this.v = v;
     }
 
     @Override
@@ -29,19 +32,25 @@ public class PagerAdapter extends FragmentPagerAdapter{
             return null;
         switch (position){
             case 0:
+                v.setText("축구");
                 curFragment = new SoccerFragment();
                 break;
             case 1:
+                v.setText("야구");
                 curFragment = new BaseballFragment();
                 break;
             case 2:
+                v.setText("배드민턴");
                 curFragment = new BadmintonFragment();
                 break;
             case 3:
+                v.setText("농구");
                 curFragment = new BasketballFragment();
                 break;
             case 4:
+                v.setText("LOL");
                 curFragment = new LOLFragment();
+
                 break;
 
         }
@@ -50,6 +59,7 @@ public class PagerAdapter extends FragmentPagerAdapter{
 
     @Override
     public int getCount() {
-        return 0;
+        return MAX_PAGE;
     }
+
 }
