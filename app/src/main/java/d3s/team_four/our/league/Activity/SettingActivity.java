71fdaae196c1e7d1f22465623fc688dbcd5c6119
hyemdooly <hyemdooly@gmail.com>
2017-02-05 07:20:08 +0900

    zsdf

--- app/src/main/java/d3s/team_four/our/league/Activity/SettingActivity.java
+++ app/src/main/java/d3s/team_four/our/league/Activity/SettingActivity.java
@@ -1,6 +1,7 @@
 package d3s.team_four.our.league.Activity;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
 import d3s.team_four.our.league.R;
@@ -11,5 +12,8 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("설정");
     }
 }
