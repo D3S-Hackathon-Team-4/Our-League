74764709a4fbc3591e845b17b1d0768f09938acc
hyemdooly <hyemdooly@gmail.com>
2017-02-04 18:16:30 +0900

    first

---
+++ app/src/main/java/d3s/team_four/our/league/MainActivity.java
@@ -0,0 +1,13 @@
+package d3s.team_four.our.league;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
